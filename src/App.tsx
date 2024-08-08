9b0f8e4c0b6c88fcfa45cf5d9f4d22990f711377
Gustavo Lázaro <gustavo.lazar@saga.com.br>
2024-08-08 11:36:16 -0300

    tailwind and custom colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-lg text-red-900">salve</h1>
+      <h1 className="text-lg text-yellow-500">salve</h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "var(--white)",
+        "gray-100": "var(--gray-100)",
+        "gray-300": "var(--gray-300)",
+        "gray-400": "var(--gray-400)",
+        "gray-500": "var(--gray-500)",
+        "gray-600": "var(--gray-600)",
+        "gray-700": "var(--gray-700)",
+        "gray-800": "var(--gray-800)",
+        "gray-900": "var(--gray-900)",
+        "green-300": "var(--green-300)",
+        "green-500": "var(--green-500)",
+        "green-700": "var(--green-700)",
+        "red-500": "var(--red-500)",
+        "red-700": "var(--red-700)",
+        "yellow-500": "var(--yellow-500)",
+      },
+    },
   },
   plugins: [],
 };
